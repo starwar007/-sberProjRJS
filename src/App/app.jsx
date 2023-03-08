badbdcf4078701092e563dbfafd8522f67c3edbe
HOME-PC <lexa_leks@mail.ru>
2023-03-08 18:41:24 +0300

    added post list

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -4,12 +4,15 @@ import api from '../utils/api';
 import cn from "classnames";
 import Main from '../components/Main/Main';
 import Header from '../components/Header/Header';
+import PostList from '../components/PostList/post-list'
 
 const App = () => {
   return (
     <>
       <Header></Header>
-      <Main></Main>
+      <Main>
+          <PostList/>
+      </Main>
     </>
     
   )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import api from "../../utils/api";
+import PostList from '../PostList/post-list';
 
 const Main = () => {
     api.getPosts().then(res => console.log(res))
     return (
-       <main className="main">Hello posts</main> 
+       <main className="main">Hello posts
+        <PostList/>
+       </main> 
     )
  
 }
