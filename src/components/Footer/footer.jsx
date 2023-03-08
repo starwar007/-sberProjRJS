b9aedceaeb4f0f648fd130ce88da1920c9642b2a
chernyshevjuv <chernyshevjuv@yandex.ru>
2023-03-08 15:54:10 +0300

    2023.03.08header+footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer1">
+        <div>Авторы проекта: </div>
+      </div>
+      <div className="footer1">
+        <a href="https://github.com">Чернышев Юрий - GitHub</a>
+        <a href="https://github.com/MikeChe419">Лях Михаил - GitHub</a>
+        <a href="https://github.com">Васильев Алексей - GitHub</a>
+        <a href="https://github.com/AndreyT-v">Тумбаев Андрей - GitHub</a>
+       </div> 
+      <div className="footer1">   
+         <div>2023 ©</div>
+      </div>   
+    </footer>
+  );
+};
+
+export default Footer;
