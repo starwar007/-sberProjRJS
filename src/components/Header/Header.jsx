fe50dc70e7c3abfd4aef003496a74c327b858ba8
Mike <amenhottep@gmail.com>
2023-03-28 17:46:06 +0300

    modified header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -32,16 +32,20 @@ export function Header() {
                   src='https://cdn2.iconfinder.com/data/icons/computer-science-butterscotch-vol-2-1/512/Programming-1024.png' />
                 <h1 className={styles.title}>Реактивные посты</h1>
               </div>
-
-
-              {/* <Search/> */}
-                <Search/>
-                <Button title="Добавить пост"  fn ={()=>setModalActive(true)} className={styles.buttonLong}/>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                { (currentUser) ? <span>{currentUser} </span> : <> 
-                  <Button title="Авторизоваться" route="/authorization" className={styles.buttonLong}/>
-                  <Button title="Зарегистрироваться" route="/registration" className={styles.buttonLong}/> 
-                </>  
+              <div className={styles.header_user}>
+                { (currentUser) ?
+                <>
+                  <Search/>
+                  <Button title="Добавить пост"  fn ={()=>setModalActive(true)} className={styles.buttonLong}/>
+                  <div className={styles.userdata_wrapper}> 
+                    <span className={styles.username}>{currentUser}</span>  
+                    <Button title="Выйти"  fn = {()=> localStorage.removeItem('token')} className={styles.buttonLong}/>
+                  </div> 
+                </>: 
+                  <div> 
+                    <Button title="Авторизоваться" route="/authorization" className={styles.buttonLong}/>
+                    <Button title="Зарегистрироваться" route="/registration" className={styles.buttonLong}/> 
+                  </div>  
                 }
               </div>
               <ModalPost active={modalActive} setActive={setModalActive}>
